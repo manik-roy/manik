518e71b892c5bd5285ae4cd17f0cc1990092ff47
manik89 <cm.dpi15@gmail.com>
2020-02-26 14:09:22 +0600

    add about section

--- index.js
+++ index.js
@@ -1,28 +1,36 @@
-$(document).ready(function(){
+/* eslint-disable */
+$(document).ready(function() {
     $win = $(window);
     $navbar = $('#header');
-    $toogle = $('.toogle-button')
-    var width = $navbar.width();
-    toogle_onclik($win, $navbar, width)
-    $win.resize(function(){
-        toogle_onclik($win, $navbar, width)
-    })
+    $toogle = $('.toogle-button');
+    let width = $navbar.width();
+    toogle_onclik($win, $navbar, width);
+    $win.resize(function() {
+        toogle_onclik($win, $navbar, width);
+    });
 
-    $toogle.click(function(){
-        $navbar.toggleClass('toogle-left')
-    })
-})
+    $toogle.click(function() {
+        $navbar.toggleClass('toogle-left');
+    });
+});
 
 function toogle_onclik($win, $nabvar, width) {
-    if($win.width() <= 768) {
-        $nabvar.css({left:`-${width}px`})
+    if ($win.width() <= 768) {
+        $nabvar.css({ left: `-${width}px` });
     } else {
-        $nabvar.css({left:'0px'})
+        $nabvar.css({ left: '0px' });
     }
 }
 // type js
-var typed = new Typed('#typed', {
-    strings: ["Front End Developer.", "a Frelencer"],
-    typeSpeed: 10,
-    loop: true
-  });
+let typed = new Typed('#typed', {
+    strings: ['Front End Developer.', 'a Freelncer'],
+    typeSpeed: 50,
+    backSpeed: true,
+    loop: true,
+});
+let typed2 = new Typed('#typed2', {
+    strings: ['Developer.', 'Freelancer'],
+    typeSpeed: 50,
+    backSpeed: true,
+    loop: true,
+});
